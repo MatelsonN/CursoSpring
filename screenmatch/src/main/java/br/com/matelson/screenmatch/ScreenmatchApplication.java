4f81ad7f5a808b79bd9a4f6acb9627bf94308a4b
Matelson Nunes <matelson.nunes@gmail.com>
2023-10-19 07:28:49 -0300

    Update ScreenmatchApplication
    Alteração da chave apikey e acrescentando o consumo api por season e criando uma lista da temporada para guardar os dados.

--- screenmatch/src/main/java/br/com/matelson/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/matelson/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.matelson.screenmatch;
 
+import br.com.matelson.screenmatch.model.DadosEpisodio;
 import br.com.matelson.screenmatch.model.DadosSerie;
+import br.com.matelson.screenmatch.model.DadosTemporada;
 import br.com.matelson.screenmatch.service.ConsumoApi;
 import br.com.matelson.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,10 +22,24 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=a5d19064");
 		System.out.println(json);
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1" +
+				"&episode=2&apikey=a5d19064");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i +
+					"&apikey=a5d19064");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
