19ebe4d6c6150e2b09845d686ca1aaabc740ba9b
Matelson Nunes <matelson.nunes@gmail.com>
2023-11-13 19:31:29 -0300

    Update Principal
    Acrescentando a opção de escolher o ano e filtrar na formatação de data brasileira com o DateTimeFormatter.

--- screenmatch/src/main/java/br/com/matelson/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/matelson/screenmatch/principal/Principal.java
@@ -7,6 +7,8 @@ import br.com.matelson.screenmatch.model.Episodio;
 import br.com.matelson.screenmatch.service.ConsumoApi;
 import br.com.matelson.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -58,6 +60,22 @@ public class Principal {
                 ).collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano deseja ver os episódios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter fomatadorData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episódio: " + e.getTitulo() +
+                                    " Data lançamento: " + e.getDataLancamento().format(fomatadorData)
+                ));
     }
 
 }
